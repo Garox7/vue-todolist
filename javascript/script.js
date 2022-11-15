d5393f5413895cf9552897f957ed67217ea6f4a6
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-11-15 19:44:05 +0100

    Added "done" functionality

--- javascript/script.js
+++ javascript/script.js
@@ -36,6 +36,19 @@ new Vue ({
             } else {}
         },
 
+        doneTodo(index) {
+            if (this.todos[index].done === 'false') {
+                this.todos[index].done = 'true';
+                
+                console.log('todo completata'); //DEBUG
+
+            } else if (this.todos[index].done === 'true') {
+                this.todos[index].done = 'false'
+
+                console.log('todo da completare'); //DEBUG
+            }
+        },
+
         deleteTodo(index) {
             this.todos.splice(index, 1);
         }
